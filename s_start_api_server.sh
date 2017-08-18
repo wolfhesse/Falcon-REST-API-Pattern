ba3cf83a1e4f216bfb95b3c7b46e44d791a2f2f3
rogera <wolfgang.schuessel@gmail.com>
2017-08-18 11:22:16 +0200

    new env in /opt/virtualenv.d port reloc to 8002

--- s_start_api_server.sh
+++ s_start_api_server.sh
@@ -1,2 +1,3 @@
+#!/usr/bin/env bash
 #gunicorn --bind 0.0.0.0 --workers 128 app:api >/dev/null
-gunicorn --bind 0.0.0.0 --workers 4 app:api >/dev/null
+gunicorn --bind 0.0.0.0:8002 --workers 4 app:api >/dev/null
